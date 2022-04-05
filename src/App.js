768d2e31aa8b6b4b8b87e133e20297c7184006b5
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-04-05 22:27:33 +0200

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Layout from './Layout';
+// import Container from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Layout/> 
+    </div>
+  );
+}
+
+export default App;
